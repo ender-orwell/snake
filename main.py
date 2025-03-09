5d19e6878cfdb7d0a4ca9dea56480dbe7b8db914
ender-orwell <enderOrwell@outlook.com>
2025-03-09 15:37:02 +0100

    we have food and the snake grows when it eats

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from turtle import Turtle, Screen
+from scoreboard import Scoreboard
+from turtle import Screen
 from snake import Snake 
+from food import Food
 import time
+
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -10,6 +13,9 @@ screen.tracer(0)
 
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -17,10 +23,26 @@ screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
 game_is_on = True
+
 while game_is_on:
     screen.update()
     time.sleep(0.1)
 
     snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect collision with wall
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail
+    if snake.head.xcor() == 9000:
+        pass
 
 screen.exitonclick()
